1b7cc0633901b9d14b6929c8679e19295cd090b3
Eric Julianto <algonacci1@gmail.com>
2023-02-10 12:22:59 +0700

    finishing

--- script.js
+++ script.js
@@ -1,19 +1,52 @@
-const img = document.getElementById("img");
-mobilenet.load().then((model) => {
-  model.classify(img).then((predictions) => {
-    console.log("Predictions: ");
-    console.log(predictions);
-    const prediction = document.createElement("p");
-    const text = document.createTextNode("Prediction: ");
-    const node = document.createTextNode(predictions[0].className);
-    const space = document.createElement("br");
-    const node2 = document.createTextNode(predictions[0].probability);
+var loadFile = function (event) {
+  var output = document.getElementById("output");
+  output.src = URL.createObjectURL(event.target.files[0]);
+};
 
-    prediction.appendChild(text);
-    prediction.appendChild(node);
-    prediction.appendChild(space);
-    prediction.appendChild(node2);
-    const element = document.getElementById("prediction");
-    element.append(prediction);
+const predict = () => {
+  const img = document.getElementById("output");
+
+  img.classList.add("animate-spin");
+
+  mobilenet.load().then((model) => {
+    model.classify(img).then((predictions) => {
+      console.log("Predictions: ");
+      console.log(predictions);
+      img.classList.remove("animate-spin");
+      displayPredictions(predictions);
+    });
   });
-});
+};
+
+const displayPredictions = (predictions) => {
+  const predictionTableBody = document.getElementById("prediction-table-body");
+  predictionTableBody.innerHTML = "";
+
+  // Create header
+  const headerRow = document.createElement("tr");
+  const classNameHeader = document.createElement("th");
+  classNameHeader.textContent = "Class Name";
+  headerRow.appendChild(classNameHeader);
+
+  const probabilityHeader = document.createElement("th");
+  probabilityHeader.textContent = "Probability";
+  headerRow.appendChild(probabilityHeader);
+
+  predictionTableBody.appendChild(headerRow);
+
+  // Add predictions data as rows
+  predictions.forEach((prediction) => {
+    const row = document.createElement("tr");
+    const classNameCell = document.createElement("td");
+    classNameCell.classList.add("p-2", "text-center");
+    classNameCell.textContent = prediction.className;
+    row.appendChild(classNameCell);
+
+    const probabilityCell = document.createElement("td");
+    probabilityCell.classList.add("p-2", "text-center");
+    probabilityCell.textContent = prediction.probability;
+    row.appendChild(probabilityCell);
+
+    predictionTableBody.appendChild(row);
+  });
+};
